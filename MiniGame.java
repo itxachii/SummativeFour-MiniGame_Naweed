d95248f4c6562f02c9af71ef92ce600a12146301
itxachii <na8zj06@limestone.on.ca>
2023-05-29 09:31:01 -0400

    Update MiniGame.java

--- MiniGame.java
+++ MiniGame.java
@@ -1,19 +1,39 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
+
 
 public class MiniGame {
     public static void main(String[] args) {
+        MiniGame game = new MiniGame();
+        game.playGame();
+    }
+
+    public void playGame() {
+        int[] numberSequence = generateNumberSequence();
+        int guessCount = 0;
+
         Scanner in = new Scanner(System.in);
-        int secretNumber = (int) (Math.random() *100) + 1;
-        int[][] guessHistory = new int[10][2];
-        int numGuesses = 0;
+        System.out.println("Welcome to the GuessTheNumber Game!");
+        System.out.println("Guess the " + numberSequence.length + " numbers in the sequence.");
 
-        System.out.println("Welcome to the Guess the Number Game!");
+        boolean guessedCorrectly = false;
+        while (!guessedCoreectly) {
+            System.out.println("Enter your guess (seperated by spaces): ");
+            String input = in.nextLine();
 
-        while (numGuesses < 10) {
-            System.out.println("Enter your Guess (1-100): ");
-            int guess = in.nextInt();
+            String[] guessArray = input.split(" ");
+
+            if (guessArray.length != numberSequence.length) {
+                System.out.println("Invalid guess length. Try again.");
+               
+            }
+
+            int[] guess = new int[guessArray.length];
+            
+
 
         }
-
     }
+
 }
