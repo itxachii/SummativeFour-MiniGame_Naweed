2c43a6498904218756730ba4879ea2f3806a0c33
itxachii <na8zj06@limestone.on.ca>
2023-05-30 09:35:11 -0400

    update minigame.2

--- MiniGame.java
+++ MiniGame.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class MiniGame {
     public static void main(String[] args) {
         MiniGame game = new MiniGame();
@@ -26,14 +25,36 @@ public class MiniGame {
 
             if (guessArray.length != numberSequence.length) {
                 System.out.println("Invalid guess length. Try again.");
-               
+
             }
 
             int[] guess = new int[guessArray.length];
-            
+            if (!parseGuess(guessArray.length)) {
+                System.out.println("Invalid guess format. Try Again. ");
+            }
 
+            guessCount++;
 
+            guessedCorrectly = checkGuess(guess, numberSequence);
+
+            if (guessedCorrectly) {
+                System.out.println("Congratulations! You guessed correctly in " + guessCount + " attempts.");
+            } else {
+                System.out.println("Incorrect guess. Try again.");
+            }
         }
+        in.nextLine();
+        System.out.println("Press enter to exit...");
+        in.nextLine();
+
     }
 
+    public int[] generateNumberSequence() {
+        Random random = new Random();
+        int[] sequence = new int[4];
+    }
+    for(int i = 0;i<sequence.length;i++)
+    {
+        sequence[i] = random.nextInt(10);
+    }return sequence;
 }
