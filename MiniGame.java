2aa5616dcefe3200678eda4c621b7bbbcec73846
itxachii <na8zj06@limestone.on.ca>
2023-05-31 08:52:20 -0400

    Updated Version

--- MiniGame.java
+++ MiniGame.java
@@ -17,20 +17,21 @@ public class MiniGame {
         System.out.println("Guess the " + numberSequence.length + " numbers in the sequence.");
 
         boolean guessedCorrectly = false;
-        while (!guessedCoreectly) {
-            System.out.println("Enter your guess (seperated by spaces): ");
+        while (!guessedCorrectly) {
+            System.out.println("Enter your guess (separated by spaces): ");
             String input = in.nextLine();
 
             String[] guessArray = input.split(" ");
 
             if (guessArray.length != numberSequence.length) {
                 System.out.println("Invalid guess length. Try again.");
-
+                
             }
 
             int[] guess = new int[guessArray.length];
-            if (!parseGuess(guessArray.length)) {
+            if (!parseGuess(guessArray, guess)) {
                 System.out.println("Invalid guess format. Try Again. ");
+                
             }
 
             guessCount++;
@@ -43,18 +44,33 @@ public class MiniGame {
                 System.out.println("Incorrect guess. Try again.");
             }
         }
+
         in.nextLine();
         System.out.println("Press enter to exit...");
         in.nextLine();
-
     }
 
     public int[] generateNumberSequence() {
         Random random = new Random();
         int[] sequence = new int[4];
+        for (int i = 0; i < sequence.length; i++) {
+            sequence[i] = random.nextInt(10);
+        }
+        return sequence;
     }
-    for(int i = 0;i<sequence.length;i++)
-    {
-        sequence[i] = random.nextInt(10);
-    }return sequence;
+
+    public boolean checkGuess(int[] guess, int[] numberSequence) {
+        return Arrays.equals(guess, numberSequence);
+    }
+
+    public boolean parseGuess(String[] guessArray, int[] guess) {
+        for (int i = 0; i < guessArray.length; i++) {
+            try {
+                guess[i] = Integer.parseInt(guessArray[i]);
+            } catch (NumberFormatException exception) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
